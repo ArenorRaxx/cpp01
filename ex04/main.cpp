31d8b3a825c59a25fac399fb508a877a194b6b52
ArenorRaxx <mateocorso@gmail.com>
2023-02-01 10:30:24 +0100

    Basic setup of ex04

---
+++ ex04/main.cpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mcorso <mcorso@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/01 10:16:20 by mcorso            #+#    #+#             */
+/*   Updated: 2023/02/01 10:30:15 by mcorso           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string>
+#include <fstream>
+
+int	main(int argc, char **argv)
+{
+}
+
+static void	replace(std::string s1, std::string s2, std::string &file_name)
+{
+	std::ifstream	original_file;
+}
